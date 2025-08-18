5cbe866adc458f7482e1a66310b7e9af42e4d86f
Eks1azy <artemvip827@gmail.com>
2025-08-18 22:28:18 +0300

    update 5.2

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ import os
 
 
 ############################################################
-API_TOKEN = '0'  # TG API BOT TOKEN
-ALLOWED_USER_ID = 0  # TG USER ID 
+API_TOKEN = '////'  # TG API BOT TOKEN
+ALLOWED_USER_ID = ////  # TG USER ID 
 ############################################################
 
 MAX_MESSAGE_LENGTH = 4000  # telegram limit – 4096 characters
